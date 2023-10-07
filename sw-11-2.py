29fc3ea748ed03980c996c64049a22a4b335922c
Vadim <ivannikov_vadimka@mail.ru>
2023-10-08 02:13:23 +0500

    Тема-10

---
+++ sw-11-2.py
@@ -0,0 +1,12 @@
+def fib(n):
+    a = 1
+    b = 1
+    for i in range(n):
+        result = a
+        a, b = b, a + b
+        yield result
+
+with open('./file/fib.txt', 'a') as file:
+    for value in fib(200):
+        file.write(f'{value}\n')
+        print(value)
